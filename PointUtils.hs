79eeb0fa0219e5ea26e1a040af1872228dfbe54d
Elias Bernardo <ebmm01@gmail.com>
2021-02-24 13:53:39 -0300

    Adding euclidianDistance and renaming file
    Co-authored-by: Daniel Marques <danielmarques7@hotmail.com>

---
+++ PointUtils.hs
@@ -0,0 +1,19 @@
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+
+data Point = Point { x :: Float, y :: Float } deriving Show
+
+points = [Point 4 4, Point 1 1, Point 2 2, Point 3 3, Point 5 5, Point 6 6]
+
+euclidianDistance :: Point -> Point -> Float
+euclidianDistance p1 p2 = sqrt (dx*dx + dy*dy)
+    where
+        dx = (x p1) - (x p2)
+        dy = (y p1) - (y p2)
+
+sortByX :: [Point] -> [Point]
+sortByX = sortBy (comparing x)
+
+sortByY :: [Point] -> [Point]
+sortByY = sortBy (comparing y)
