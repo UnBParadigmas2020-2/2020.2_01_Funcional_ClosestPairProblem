e82543f1cc74b26ea96cfdf25ce15d355e10c13e
Fernando Miranda <71859307+ilus1@users.noreply.github.com>
2021-02-24 17:29:36 -0300

    Update leitura.hs

--- leitura.hs
+++ leitura.hs
@@ -1,6 +1,16 @@
-
-
 main = do  
     arquivo <- readFile("testes.txt")
     let lista = (map (map read . words) . lines) arquivo :: [[Int]]
+    separaLista lista
     print lista
+
+
+separaLista [] = return()
+separaLista [[]] = return()
+separaLista (h:t) = do
+    let passo = montaLista (head h) t
+    print passo
+    separaLista (drop (head h) t)
+
+montaLista 0 lista = []
+montaLista n (h:t) = h: montaLista(n-1) t
