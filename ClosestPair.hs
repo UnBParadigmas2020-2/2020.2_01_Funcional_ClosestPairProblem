1044e63a5942b351ee90ccb0d14c9e4ae78d5818
Elias Bernardo <ebmm01@gmail.com>
2021-02-24 21:09:32 -0300

    Adding halveArray function
    Co-authored-by: Daniel Marques <danielmarques7@hotmail.com>

--- ClosestPair.hs
+++ ClosestPair.hs
@@ -6,10 +6,16 @@ getMiddleIndexFromList lst = ceiling (fromIntegral(length lst) / 2) - 1
 getMiddleItemFromList :: [a] -> a
 getMiddleItemFromList lst = lst !! getMiddleIndexFromList lst
 
+halveArray :: [a] -> ([a], [a])
+halveArray a = (x, y)
+    where 
+        x = take ((getMiddleIndexFromList a) + 1) a
+        y = drop ((getMiddleIndexFromList a) + 1) a
+
 
 -- 10001 por ser o valor definido no URI
 closestPair :: [Point] -> Int
 closestPair [] = 10001
 closestPair [a] = 10001
-closestPair lst = length lst
+closestPair lst = 0
     
