adfc22c3cb6051b40eed2ef1752cd63598b019df
danielmarques28 <danielmarques7@hotmail.com>
2021-02-25 15:32:36 -0300

    Change sortBy to sortOn
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- PointUtils.hs
+++ PointUtils.hs
@@ -1,19 +1,17 @@
 module PointUtils where
 
-import Data.List (sortBy)
-import Data.Ord (comparing)
+import Data.List (sortOn)
 
-
-data Point = Point { x :: Float, y :: Float } deriving Show
+data Point = Point { x :: Float, y :: Float } deriving (Eq, Ord, Show)
 
 euclidianDistance :: Point -> Point -> Float
 euclidianDistance p1 p2 = sqrt (dx*dx + dy*dy)
     where
-        dx = (x p1) - (x p2)
-        dy = (y p1) - (y p2)
+        dx = x p1 - x p2
+        dy = y p1 - y p2
 
 sortByX :: [Point] -> [Point]
-sortByX = sortBy (comparing x)
+sortByX = sortOn x
 
 sortByY :: [Point] -> [Point]
-sortByY = sortBy (comparing y)
+sortByY = sortOn y
