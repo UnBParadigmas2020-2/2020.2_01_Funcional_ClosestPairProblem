959dcbbc75dbbe7841ac26001e0698a7d854bc75
danielmarques28 <danielmarques7@hotmail.com>
2021-02-25 19:25:40 -0300

    Create DivideAndConquer module structure
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

---
+++ DivideAndConquer.hs
@@ -0,0 +1,17 @@
+module DivideAndConquer where
+
+import PointUtils
+import ClosestPair
+
+divideAndConquer points left right = 10001
+    where
+        middle = getMiddleIndexFromList points
+        dacLeft = divideAndConquer points left middle
+        dacRight = divideAndConquer points (middle + 1) right
+
+calcClosestDistanceFromInputDAC :: [Point] -> Float
+calcClosestDistanceFromInputDAC [] =  10001
+calcClosestDistanceFromInputDAC [points] =  10001
+calcClosestDistanceFromInputDAC points =  divideAndConquer points 0 lastIndex
+    where
+        lastIndex = length points - 1
