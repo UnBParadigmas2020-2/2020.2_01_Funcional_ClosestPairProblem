95b9546ac504116f8e6bb78e5e1fe3413c518cd8
Elias Bernardo <ebmm01@gmail.com>
2021-02-25 10:48:28 -0300

    Adding Main
    Co-authored-by: Daniel Marques <danielmarques7@hotmail.com>

---
+++ Main.hs
@@ -0,0 +1,6 @@
+import BruteForce
+import PointUtils
+
+points = [Point 0 2, Point 6 67, Point 43 71, Point 39 107, Point 189 140]
+
+main = print (show (calcClosestDistanceFromInputBruteForce points))
