f116fab91cc40db49eacccd7d0541c3c5b37adc9
Fernando Miranda <71859307+ilus1@users.noreply.github.com>
2021-02-26 18:12:07 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,19 +1,12 @@
 
-import Conversion
-
 main = do  
     file <- readFile("testes.txt")
     let list = (map (map read . words) . lines) file ::[[Int]]
-    splitCases list
+    splitTests list
     print list
 
 
-splitCases [] = return()
-splitCases (h:t) = do
-    let pointsList = getPointsList (head h) t
-    print(map listToPoint pointsList)
-    splitCases(drop (head h) t)
-
-
-getPointsList 0 lista = []
-getPointsList n (h:t) = h: getPointsList(n-1) t
+splitTests [] = return()
+splitTests (h:t) = do
+    print((map listToPoint (splitList (head h) t)))
+    splitTests(drop (head h) t)
