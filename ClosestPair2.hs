7ba5dc87c92e1510badfa4aa9c54572bce81086a
lorryaze <lorrany90@gmail.com>
2021-02-27 04:44:41 -0300

    Tryng to make recursive closest pair

--- ClosestPair2.hs
+++ ClosestPair2.hs
@@ -1,41 +1,40 @@
---module ClosestPair
---( getMiddleIndexFromList
---, getMiddleItemFromList
---, halveArray
---) where
-
---calcula o tamanho da metade da lista
---sortByX :: [Point] -> [Point]
---sortByX = sortOn x
-
---sortByY :: [Point] -> [Point]
---sortByY = sortOn y
-
+import PointUtils
 import PointUtils (Point(..))
 points = [Point 0 2, Point 6 67, Point 43 71, Point 39 107, Point 189 140]
-
-length' :: (Num b) => [a] -> b 
-length' [] = 0 
-length' xs = sum [1 | _ <- xs]
+--points list order by x 
+px = sortByX points
+--points list order by y
+py = sortByY points
 
 getMiddleIndexFromList :: [a] -> Int
 getMiddleIndexFromList [] = 0
 getMiddleIndexFromList lst = ceiling (fromIntegral(length lst) / 2)
 
-mid = getMiddleIndexFromList points
 leftList :: [a] -> [a]
 leftList a = x
     where 
-        x = take (mid) a
-
---o tamanho da minha lista r = points len - leftList len
+        x = take (getMiddleIndexFromList a) a
 
 rightList :: [a] -> [a]
 rightList a = y
     where 
-        y = drop (mid) a  
+        y = drop (getMiddleIndexFromList a) a  
+
+--calcula o tamanho de uma lista
+length' :: (Num b) => [a] -> b 
+length' [] = 0 
+length' xs = sum [1 | _ <- xs]
+
+--divide ordered px list in left and right lists
+let l = leftList px
+let r = rightList px
+--sort l and r both by x and y coordinates
+let lx = sortByX l
+let ly = sortByY l
+let rx = sortByX r
+let ry = sortByY r
+
+
 
 main = print(getMiddleIndexFromList points)
-l = leftList points
-r = rightList points
---left = print(leftList points mid) 
+
